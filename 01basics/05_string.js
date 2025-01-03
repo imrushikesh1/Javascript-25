4aaa8697552a1e1b7b4c1431408cd3e529984e43
imrushikesh1 <thisisrushikeshjadhav@gmail.com>
2025-01-03 20:15:23 +0530

    string&math

---
+++ 01basics/05_string.js
@@ -0,0 +1,26 @@
+const name = "Rushikesh";
+const count = 50;
+
+console.log(`My Name is ${name} and repo count is ${count}`);
+
+const gameName = new String('hitesh-hc-com')
+
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.__proto__);
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const gameeName = "chessGame";
+
+// const newString = gameeName.substring(0, 4);
+// console.log(newString);
+
+// const anotherString = gameeName.slice(-4,-11);
+// console.log(anotherString);
+
+let min = 20;
+let max = 40;
+
+console.log(Math.floor(Math.random() * ((max - min)+1) + min));
