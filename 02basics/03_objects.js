b2bf5167273e2f9396bef4d0e7e3da69e6fefe39
imrushikesh1 <thisisrushikeshjadhav@gmail.com>
2025-01-06 21:33:50 +0530

    6jan

---
+++ 02basics/03_objects.js
@@ -0,0 +1,42 @@
+//singleton
+// Object.create using constructor
+
+// object literals
+
+const mySym = Symbol("key1");
+
+const JsUser = {
+    name: "rushi",
+    age: 18,
+[mySym]: "Mykey1",
+    "fullname":"Rushi Jadhav",
+    location: "Pune",
+    email: "rushi@js.com",
+    isLoggedIn: false,
+    lastLoginDays:["Monday","Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+
+console.log(JsUser["fullname"]);
+
+console.log(JsUser[mySym]);
+
+console.log(JsUser);
+
+JsUser.email = "rushi@google.com"; 
+// Object.freeze(JsUser);
+JsUser.email = "rushi@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function () {
+    console.log("Hello Js USer");
+}
+
+JsUser.greetingTwo = function () {
+    console.log(`Hello JS Users,${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
