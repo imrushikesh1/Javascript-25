14c559491d17763e233cff8a5a1e6d915fe8165c
imrushikesh1 <thisisrushikeshjadhav@gmail.com>
2025-01-10 19:46:38 +0530

    Dom-10JAN

---
+++ 06Dom/script.js
@@ -0,0 +1,9 @@
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+
+let divs = document.querySelectorAll(".box");
+console.log(divs);
+
+divs[0].innerText = "New unique value1 ";
+divs[1].innerText = "New unique value2 ";
+divs[2].innerText = "New unique value3 ";
